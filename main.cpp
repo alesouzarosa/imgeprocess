729b9beb9cc3505d246931c52a72831ebb293bb7
alesouzarosa <43153497+alesouzarosa@users.noreply.github.com>
2019-10-10 11:52:13 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// TE3 trabalho 1 2 e 3
+// Alenxadre de Souza Rosa
+//T1 distancias
+//T2 convolucao
+//T3 bordas e gradientes
+
+// funcoes basicas
+struct imagem leitura(ponteiro imagem);
+bool salvar(strutura imagem, tipo);
+struck imagemGray escalacinza(struck imagemRGB)
+
+//T1
+struct imagemBit resultado(imagemRGB, 
